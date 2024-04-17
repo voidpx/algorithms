025caebbedf10057cd1c85be04fc93bccca96d54
Sam Zheng <voidp@outlook.com>
2024-04-17 23:41:19 +0800

    test

--- test/test_fft.c
+++ test/test_fft.c
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <time.h>
 #include "../src/fft.h"
 #include "minunit.h"
 
@@ -21,26 +23,25 @@ static void print_comp(char*head, complex *v, uint32_t len) {
 	printf("\n");
 }
 
-static void test_fft() {
-#define N 1000
-	double data[N];
-	double s = 1.0/N;
+static double *test_data(uint32_t n) {
+	double *data = malloc(sizeof(double) * n);
+	double s = 1.0/n;
 
-	for (int i = 0; i < N; ++i) {
+	for (int i = 0; i < n; ++i) {
 		double t = i*s;
 		double y = 3 * sin(2 * M_PI * t);
 		y += sin(2 * M_PI * 4 * t);
 		y += 0.5 * sin(2 * M_PI * 7 * t);
 		data[i] = y;
 	}
+	return data;
+}
 
-//	print("data", data, N);
-
-	FFT p = dft(data, N);
+static void test_fft(double *data, uint32_t n, FFT p) {
+//	print("data", data, n);
 //	print_comp("DFT", p.value, p.len);
 
-
-	FFT pf = fft(data, N);
+	FFT pf = fft(data, n);
 //	print_comp("FFT", pf.value, pf.len);
 
 	assert(p.len == pf.len);
@@ -50,17 +51,66 @@ static void test_fft() {
 
 	double *d = ifft(pf);
 
-//	print("IFFT:", d, N);
+//	print("IFFT:", d, n);
 
-	for (int i = 0; i < N; ++i) {
+	for (int i = 0; i < n; ++i) {
 		assert(abs(d[i] - data[i]) < 10e-8);
 	}
 
 	free(d);
-	free(p.value);
 	free(pf.value);
 }
 
+typedef union TR {
+	FFT fft;
+	double * real;
+} TR;
+
+typedef struct func {
+	TR (*f)(va_list);
+} func;
+
+static TR time_it(char *s, func f, ...) {
+	struct timespec t0, t1;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+	va_list va;
+	va_start(va, f);
+	TR r = f.f(va);
+	va_end(va);
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+	double t = (t1.tv_sec - t0.tv_sec) + ((double)(t1.tv_nsec - t0.tv_nsec))/1.0e+9;
+	printf("%s, time: %.6fs\n", s, t);
+	return r;
+}
+
+static FFT tf(FFT (*f)(double *, uint32_t), va_list a) {
+	double *data = va_arg(a, double*);
+	uint32_t n =  va_arg(a, uint32_t);
+	return f(data, n);
+}
+
+static TR dftf(va_list a) {
+	return (TR){tf(dft, a)};
+}
+
+static TR fftf(va_list a) {
+	return (TR){tf(fft, a)};
+}
+
 int main() {
-	test_fft();
+#define N 2000
+	struct timespec t0, t1;
+	double *data = test_data(N);
+	func df = {dftf};
+	TR dftr = time_it("DFT", df, data, (uint32_t)N);
+
+	// check correctness
+	test_fft(data, N, dftr.fft);
+
+	func ff = {fftf};
+	TR fftr = time_it("FFT", ff, data, (uint32_t)N);
+
+	free(dftr.fft.value);
+	free(fftr.fft.value);
+
 }
